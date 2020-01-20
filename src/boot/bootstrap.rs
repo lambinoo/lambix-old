c09050d3cf184babb906649f1fa1f826eae464ac
Lamb <contact@lambtowolf.me>
2020-01-20 22:01:30 +0100

    removing unused fn to map kernel to high addr

--- src/boot/bootstrap.rs
+++ src/boot/bootstrap.rs
@@ -29,38 +29,3 @@ unsafe fn exec_with_new_stack(f: unsafe fn() -> !) -> ! {
     f();
 }
 
-
-/*
-unsafe fn remap_kernel_and_jump(entry_point: unsafe fn() -> !) -> ! {
-    let kernel_range = kernel::kernel_range();
-    let high_range = VirtAddr::from(KERNEL_START) .. VirtAddr::from(KERNEL_END);
-
-    let max_size = usize::from(high_range.end) - usize::from(high_range.start);
-    let size = usize::from(kernel_range.end) - usize::from(kernel_range.start);
-
-    if size < max_size {
-        for offset in (0..size).step_by(PAGE_SIZE) {
-            map4k(
-                high_range.start.wrapping_add(offset),
-                kernel_range.start.wrapping_add(offset),
-                Flags::PRESENT | Flags::GLOBAL
-            ).expect("failed to map the kernel to a higher address range");
-        }
-        purge_tlb();
-    } else {
-        panic!("kernel is too big (size: {}MiB, max: {}MiB), aborting",
-            size / 1024 / 1024,
-            max_size / 1024 / 1024
-        );
-    }
-
-    let address = entry_point as *const ();
-    let offset = (address as usize) - usize::from(kernel_range.start);
-
-    let new_entry_point = core::mem::transmute::<_, fn() -> !>(
-        high_range.start.wrapping_add(offset)
-    );
-
-    exec_with_new_stack(new_entry_point);
-}
-*/
